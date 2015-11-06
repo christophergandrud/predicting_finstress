fc435a493ed010079a6c9c3cae40d0b7c90dbf65
christophergandrud <christopher.gandrud@gmail.com>
2015-11-06 13:58:42 +0100

    minor

--- analysis_data/finstress_predict_test.R
+++ analysis_data/finstress_predict_test.R
@@ -11,7 +11,7 @@ library(DataCombine)
 library(countrycode)
 library(WDI)
 library(plm)
-library(moongazer)
+library(22222)
 
 # Set working directory
 possibles <- c('/git_repositories/predicting_finstress/analysis_data')
